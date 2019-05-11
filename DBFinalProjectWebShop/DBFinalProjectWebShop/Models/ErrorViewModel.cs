268a7f891684ad7b73d71c0cbdf4edec084338b6
Daniel Bøbel <daniel.boebel@gmail.com>
2019-05-11 11:44:47 +0200

    MVC created

---
+++ DBFinalProjectWebShop/DBFinalProjectWebShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DBFinalProjectWebShop.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
